71812e50936a722ad22755111cf0815bb443744c
Orlovchikk <115371481+Orlovchikk@users.noreply.github.com>
2022-11-13 21:41:12 +0400

    Update main.js

--- main.js
+++ main.js
@@ -3,11 +3,19 @@ const numberSpan = document.querySelector('.task__number'),
   startBtn = document.querySelector('.startBtn'),
   answerBtn = document.querySelector('.submit-answer'),
   taskWindow = document.querySelector('.task__inner'),
-  answerInput = document.querySelector('.answer__input');
+  answerInput = document.querySelector('.answer__input'),
+  cats = [
+    'https://forgifs.com/gallery/d/310576-2/Otter-kitten-sink-bath.gif',
+    'https://forgifs.com/gallery/d/303115-2/Kitten-chasing-ping-pong-ball.gif',
+    'https://forgifs.com/gallery/d/380368-3/Troll-cat.gif',
+    'https://forgifs.com/gallery/d/319667-2/Cat-hind-legs-dance_001.gif',
+    'https://forgifs.com/gallery/d/306806-2/Cat-box-traps.gif'
+  ]
 
 
 let number,
-  answer
+  answer,
+  justRandomNumber;
 
 
 function start() {
@@ -21,19 +29,33 @@ function start() {
 function task() {
   number = (parseInt(Math.random() * 50)) ** 2;
   answer = Math.sqrt(number);
-  numberSpan.innerHTML = `${number}`;
+  console.log(answer);
+  numberSpan.innerHTML = `Вычислите: √${number}`;
 }
 
 function check() {
   if (answerInput.value == answer) {
-    answerInput.value = '';
-    task();
+    justRandomNumber = parseInt(Math.random() * 5);
+    numberSpan.innerHTML = `<img src="${cats[justRandomNumber]}">`;
+    setTimeout(function () {
+      answerInput.value = '';
+      task();
+    }, 5000);
   }
   else {
     alert('Попробуй еще раз');
+    answerInput.value = '';
   }
 }
 
+answerInput.value = '';
+answerInput.addEventListener("keydown", function (event) {
+  if (event.key === "Enter") {
+    check();
+  }
+});
+startBtn.addEventListener('click', start);
+answerBtn.addEventListener('click', check);
 answerInput.value = '';
 answerInput.addEventListener("keydown", function(event) {
     if (event.key === "Enter") {
